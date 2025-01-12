f036fc3942457ba28d09a451b45fbcff1051e8e9
bronya0 <tangssst@qq.com>
2025-01-12 23:53:10 +0800

    fix

--- go.mod
+++ go.mod
@@ -8,14 +8,15 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/gin-contrib/pprof v1.5.1
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-co-op/gocron/v2 v2.14.2
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.23.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-resty/resty/v2 v2.16.2
 	github.com/golang-jwt/jwt/v4 v4.5.1
+	github.com/google/uuid v1.6.0
 	github.com/pkg/errors v0.9.1
-	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.19.0
 	github.com/twmb/franz-go v1.18.0
 	github.com/twmb/franz-go/pkg/kadm v1.14.0
@@ -45,6 +46,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
@@ -56,6 +58,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- internal/jobs/init.go
+++ internal/jobs/init.go
@@ -2,31 +2,70 @@ package jobs
 
 import (
 	"gin-starter/internal/util/glog"
-	"github.com/robfig/cron/v3"
-	"log"
+	"github.com/go-co-op/gocron/v2"
+	"github.com/google/uuid"
+	"time"
 )
 
 func InitCronJob() {
-	// 自定义日志记录器
-	logger := cron.VerbosePrintfLogger(log.New(log.Writer(), "cron: ", log.LstdFlags))
+	// 创建一个新的调度器
+	s := initScheduler()
 
-	c := cron.New(
-		cron.WithLogger(logger),
-		cron.WithChain(
-			cron.Recover(logger), // 捕获 panic 并记录日志
+	// 添加一个每10秒执行一次的任务
+	//addJob(s, "print1", "*/10 * * * * *", PrintJob)
+
+	// 启动调度器
+	start(s)
+}
+
+// 通用job构造函数
+func addJob(s *gocron.Scheduler, jobName string, crontab string, function any, parameters ...any) {
+	// 超时请自己在任务中处理，不在外面做。
+	scheduler := *s
+	_, err := scheduler.NewJob(
+		gocron.CronJob(
+			crontab,
+			true,
 		),
+		gocron.NewTask(
+			function,
+			parameters...,
+		),
+		gocron.WithEventListeners(jobRecover()),
+		gocron.WithName(jobName),
+		gocron.WithTags(jobName),                             // 用于s删除
+		gocron.WithSingletonMode(gocron.LimitModeReschedule), // 避免重叠运行
 	)
 
-	// 依次是 分 时 日 月 周。非常自由
-	// @every 1s、@every 1h、@every 1m、@every 1m2s、@every 1h30m10s
-	_, err := c.AddFunc("@every 1m", PrintJob)
 	if err != nil {
+		glog.Log.Errorf("定时任务注册失败！: %v：%v", jobName, err)
 		panic(err)
 	}
-	c.Start()
+	glog.Log.Infof("定时任务: %v 注册成功", jobName)
+}
 
-	glog.Log.Info("定时任务加载成功...")
+func initScheduler() *gocron.Scheduler {
+	s, err := gocron.NewScheduler(
+		gocron.WithLocation(time.Local),
+		gocron.WithGlobalJobOptions(),
+	)
+	if err != nil {
+		glog.Log.Errorf("initScheduler失败！: %v", err)
+		panic("initScheduler失败！: " + err.Error())
+	}
+	return &s
+}
 
+func start(s *gocron.Scheduler) {
+	scheduler := *s
+	scheduler.Start()
+	glog.Log.Info("定时任务启动成功...")
+}
+
+func jobRecover() gocron.EventListener {
+	return gocron.AfterJobRunsWithPanic(func(jobID uuid.UUID, jobName string, recoverData any) {
+		glog.Log.Errorf("Job Panic！！！：jobName: %s jobID: (%s): %v\n", jobName, jobID, recoverData)
+	})
 }
 
 func PrintJob() {

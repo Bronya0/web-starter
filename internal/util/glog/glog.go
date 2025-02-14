9447710a03943d6842c6400a691691a6a96a2e90
bronya0 <tangssst@qq.com>
2025-02-15 00:52:37 +0800

    fix

--- internal/util/glog/glog.go
+++ internal/util/glog/glog.go
@@ -2,12 +2,12 @@ package glog
 
 import (
 	"gin-starter/internal/config"
-	logging "github.com/donnie4w/go-logger/logger"
+	"github.com/donnie4w/go-logger/logger"
 	"log"
 )
 
 var (
-	Log *logging.Logging
+	Log *logger.Logging
 )
 
 func InitLogger() {
@@ -15,23 +15,22 @@ func InitLogger() {
 }
 
 // InitLogger pathFile: 日志全路径
-func initLogger(path string) *logging.Logging {
+func initLogger(path string) *logger.Logging {
 	log.Println("初始化日志……" + path)
-
-	logger := logging.NewLogger()
-	logger.SetOption(&logging.Option{
-		Level:     logging.LEVEL_INFO,
+	l := logger.NewLogger()
+	l.SetOption(&logger.Option{
+		Level:     logger.LEVEL_INFO,
 		Console:   true, // 控制台输出
-		Format:    logging.FORMAT_LEVELFLAG | logging.FORMAT_SHORTFILENAME | logging.FORMAT_DATE | logging.FORMAT_MICROSECONDS,
+		Format:    logger.FORMAT_LEVELFLAG | logger.FORMAT_SHORTFILENAME | logger.FORMAT_DATE | logger.FORMAT_MICROSECONDS,
 		Formatter: "[{time}] {level} {file}: {message}\n",
 		// size或者time模式
-		FileOption: &logging.FileTimeMode{ // 这里用时间切割
-			Filename:   path,             // 日志文件路径
-			Timemode:   logging.MODE_DAY, // 按天
-			Maxbuckup:  180,              // 最多备份日志文件数
-			IsCompress: false,            // 是否压缩
+		FileOption: &logger.FileTimeMode{ // 这里用时间切割
+			Filename:   path,            // 日志文件路径
+			Timemode:   logger.MODE_DAY, // 按天
+			Maxbuckup:  180,             // 最多备份日志文件数
+			IsCompress: false,           // 是否压缩
 		},
 	})
 
-	return logger
+	return l
 }
